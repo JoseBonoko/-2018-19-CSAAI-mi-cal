343d1331533780ebb3f5ce84f226bdbb0d4fc1c5
Jose Manuel Bonoko Bijeri <jbonoko@alpha30.aulas.gsyc.urjc.es>
2019-02-25 14:07:34 +0100

    modelando calculadora II

--- pruebajava.js
+++ pruebajava.js
@@ -1,60 +1,90 @@
 function main(){
   //-- Variables
-  uno = document.getElementById('uno')
+var uno = document.getElementById('uno')
+var dos = document.getElementById('dos')
+var tres = document.getElementById('tres')
+var cuatro = document.getElementById('cuatro')
+var cinco = document.getElementById('cinco')
+var seis = document.getElementById('seis')
+var siete = document.getElementById('siete')
+var ocho = document.getElementById('ocho')
+var nueve = document.getElementById('nueve')
+var cero = document.getElementById('cero')
+var sumar = document.getElementById('sumar')
+var restar = document.getElementById('restar')
+var multiplicar = document.getElementById('multiplicar')
+var dividir = document.getElementById('dividir')
+var valor1 = 0;
+var valor2 = 0;
+var resultado = 0;
+
+  //--Acciones
   uno.onclick = () => {
     display = document.getElementById('display')
-    display.innerHTML = "1"
+    display.innerHTML +="1"
   }
-  dos = document.getElementById('dos')
   dos.onclick = () => {
     display = document.getElementById('display')
-    display.innerHTML = "2"
+    display.innerHTML += "2"
   }
-  tres = document.getElementById('tres')
   tres.onclick = () => {
     display = document.getElementById('display')
-    display.innerHTML = "3"
+    display.innerHTML += "3"
   }
-  cuatro = document.getElementById('cuatro')
   cuatro.onclick = () => {
     display = document.getElementById('display')
-    display.innerHTML = "4"
+    display.innerHTML += "4"
   }
-  cinco = document.getElementById('cinco')
   cinco.onclick = () => {
     display = document.getElementById('display')
-    display.innerHTML = "5"
+    display.innerHTML += "5"
   }
-  seis = document.getElementById('seis')
   seis.onclick = () => {
     display = document.getElementById('display')
-    display.innerHTML = "6"
+    display.innerHTML += "6"
   }
-  siete = document.getElementById('siete')
   siete.onclick = () => {
     display = document.getElementById('display')
-    display.innerHTML = "7"
+    display.innerHTML += "7"
   }
-  ocho = document.getElementById('ocho')
   ocho.onclick = () => {
     display = document.getElementById('display')
-    display.innerHTML = "8"
+    display.innerHTML += "8"
   }
-  nueve = document.getElementById('nueve')
   nueve.onclick = () => {
     display = document.getElementById('display')
-    display.innerHTML = "9"
+    display.innerHTML += "9"
   }
-  cero = document.getElementById('cero')
   cero.onclick = () => {
     display = document.getElementById('display')
-    display.innerHTML = "0"
+    display.innerHTML += "0"
+  }
+  sumar.onclick = () => {
+    display = document.getElementById('display')
+    display.innerHTML += "+"
+  }
+  restar.onclick = () => {
+    display = document.getElementById('display')
+    display.innerHTML += "-"
+  }
+  multiplicar.onclick = () => {
+    display = document.getElementById('display')
+    display.innerHTML += "*"
+  }
+  dividir.onclick = () => {
+    display = document.getElementById('display')
+    display.innerHTML += "/"
   }
   //-- Operaciones
+
+  function suma(){
+    valor1 = display.innerHTML
+  }
+  
   var gui = {
     display: document.getElementById('display'),
-    sumar: document.getElementById('suma'),
-    restar: document.getElementById('resta'),
+    sumar: document.getElementById('sumar'),
+    restar: document.getElementById('restar'),
     multiplicar: document.getElementById('multilicar'),
     dividir: document.getElementById('dividir')
   }
